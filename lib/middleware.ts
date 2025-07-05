99d65e6c9726024a12ca6b446bdda1880e351990
Yogesh <yogesh@celestialsys.com>
2025-07-05 17:27:15 +0530

    middleware for api authentication

---
+++ lib/middleware.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from "next/server";
+import { verifyAccessToken } from "./tokens";
+import jwt from 'jsonwebtoken';
+
+export type JWTPayload = {
+    [key: string]: string,
+} | string | jwt.JwtPayload;
+
+export const withAuth = async (req: NextRequest,   handler: (abc: JWTPayload ) => Promise<NextResponse>) => {
+    const token = req.cookies.get('access_token')?.value;
+    if (!token) return NextResponse.json({ message: 'No Token Provided!'}, { status: 401 });
+    const parsed = await verifyAccessToken(token);
+    if (parsed?.valid && parsed?.payload) {
+        return handler(parsed.payload);
+    } else {
+        return NextResponse.json({ message: parsed.error?.message }, { status: parsed.error?.name === 'TokenExpiredError' ? 401 : 403 });
+    }
+}
