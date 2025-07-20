7438452ac8021fdff878cb9cbcf106cdd67683f5
Yogesh <yogesh@celestialsys.com>
2025-07-20 14:20:40 +0530

    logo and logout Button

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -4,14 +4,16 @@ import { AuthContextType } from "@/context/auth";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { logoutHandler } from "@/lib/logout";
+import { LogOut } from "lucide-react";
+import { useIsMobile } from "@/hooks/use-mobile";
 
 type NavProps = {
   user: AuthContextType
 }
 
-export function Navbar(props : NavProps) {
+export function Navbar(props: NavProps) {
   const { user = null } = props
-  
+  const isMobile = useIsMobile();
 
   const logout = () => {
     logoutHandler(true);
@@ -20,12 +22,16 @@ export function Navbar(props : NavProps) {
 
   return (
     <header className="w-full h-16 bg-sidebar px-4 flex items-center justify-between">
-      <div className="text-2xl font-semibold flex-2">NoteX</div>
-      <div className="flex justify-center gap-2 flex-3">
+      {!isMobile ? (
+        <div className="text-2xl font-semibold flex-2">NoteX</div>
+      ) : (
+        <div className="text-2xl font-semibold flex-2">N<span className="text-sm">X</span></div>
+      )}
+      <div className="flex justify-center gap-2 flex-6 lg:flex-3">
         <Input placeholder="Search Notes" className="" />
       </div>
       <div className=" flex flex-2 justify-end">
-        <Button variant='outline' onClick={logout}>Logout</Button>
+        <Button variant='outline' onClick={logout}><LogOut />{!isMobile ? 'Logout' : ''}</Button>
       </div>
     </header>
   )
